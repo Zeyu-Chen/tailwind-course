9e75396866e78d7d95a38ab6211c0fc3e35c1e73
Zeyu Chen <iszeyu.chen@gmail.com>
2024-03-13 15:08:01 +1100

    feat: finish p15

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: [".{html,js}"],
+  theme: {
+    fontFamily: {
+      "sans": ["ui-sans-serif", "system-ui"],
+      "serif": ["Tapestry", "Georgia"],
+      "mono": ["ui-monospace", "SFMono-Regular"]
+    }
+  },
+  plugins: []
+};
+
